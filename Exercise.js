d663debdb0dee2571a465d3304d06758566bc6c3
Archith47 <archithreddy44@gmail.com>
2021-09-28 21:34:46 +0530

    added few more functions

--- Exercise.js
+++ Exercise.js
@@ -1,10 +1,11 @@
 let copy;
 copy = () => console.log("copied");
 let cut = () => console.log("cut");
-
+let dClick=()=> console.log("double click");
 var x = document.getElementById("myInput").value;
 function myFunction() {
     var x = document.getElementById("myInput").value;
     document.getElementById("demo").innerHTML = "You wrote: " + x;
   }
 let paste = () => console.log("paste");
+let error = ()=> alert('The image could not be loaded.');
